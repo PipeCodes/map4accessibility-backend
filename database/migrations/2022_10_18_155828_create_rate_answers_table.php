70e9e9fd41a600082089af7c423232988ce43498
Pedro Angelino <pedroangelino@Mini-de-Pedro.Home>
2022-10-19 16:04:48 +0100

    migrations done, before views

---
+++ database/migrations/2022_10_18_155828_create_rate_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rate_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rate_question_id')->references('id')->on('rate_questions')->cascadeOnDelete();
+            $table->string('body');
+            $table->string('slug');
+            $table->integer('order')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rate_answers');
+    }
+};
