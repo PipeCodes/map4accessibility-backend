8bd1bc5118892ee020ff5a1459d5e826e8d7e1e9
João Rosa <joao.rosa06@gmail.com>
2022-10-20 16:37:07 +0100

    Send email after creating user in backoffice

--- app/Filament/Resources/AppUserResource/Pages/CreateAppUser.php
+++ app/Filament/Resources/AppUserResource/Pages/CreateAppUser.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\AppUserResource\Pages;
 
 use App\Filament\Resources\AppUserResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailConfirmation;
+use Filament\Pages\Actions;
 
 class CreateAppUser extends CreateRecord
 {
     protected static string $resource = AppUserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $user = $this->record;
+        Mail::to($user->email)->send(new EmailConfirmation($user->record));
+    }
 }
