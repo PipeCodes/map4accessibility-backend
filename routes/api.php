24e68fd00c8bef117d80ba68e11c0fd3719126ec
Miguel Fortes <miguel.fortes@pipecodes.com>
2022-10-24 11:52:47 +0100

    task https://trello.com/c/EPwrEfFC/72-api-check-if-e-mail-exists

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
+
+    Route::post('/auth/check-email', [AuthController::class, 'checkEmail']);
     Route::post('/auth/register', [AuthController::class, 'register']);
     Route::post('/auth/login', [AuthController::class, 'login']);
     Route::post('/auth/login-by-provider', [AuthController::class, 'loginByProvider']);
