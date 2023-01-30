01dd672d23170e711fe3639e2cf7023ecc09d64d
Miguel Fortes <miguel.fortes@pipecodes.com>
2023-01-30 19:39:45 +0000

    google maps api places

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use phpDocumentor\Reflection\Types\This;
+use SKAgarwal\GoogleApi\PlacesApi;
 
 /**
  * @OA\Info(
@@ -37,4 +39,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected PlacesApi $googlePlaces;
+
+    public function __construct()
+    {
+        $this->googlePlaces = new PlacesApi(env('GOOGLE_MAPS_API_KEY', false));
+    }
 }
