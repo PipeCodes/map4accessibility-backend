ca87271c717dd4ee953c64f4410eef196b758e91
Ricardo Valadas <ricardo.valadas@digitalmattershq.com>
2023-07-12 12:00:12 +0100

    Add export feature to the place evaluation page

--- app/Filament/Resources/PlaceEvaluationResource.php
+++ app/Filament/Resources/PlaceEvaluationResource.php
@@ -13,6 +13,7 @@ use Filament\Tables;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class PlaceEvaluationResource extends Resource
 {
@@ -69,6 +70,7 @@ class PlaceEvaluationResource extends Resource
                 Tables\Actions\ForceDeleteAction::make(),
             ])
             ->bulkActions([
+                ExportBulkAction::make(),
                 Tables\Actions\DeleteBulkAction::make(),
                 Tables\Actions\RestoreBulkAction::make(),
                 Tables\Actions\ForceDeleteBulkAction::make(),
