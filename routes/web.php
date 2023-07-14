15dad9a550d79de7669d95034c24fdb04d15edfc
Ricardo Valadas <ricardo.valadas@digitalmattershq.com>
2023-07-14 17:02:38 +0100

    Fix redirect

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth/login');
+    return redirect('/admin');
 });
 
 Route::get('/terms-conditions', [LegalTextController::class, 'terms']);
