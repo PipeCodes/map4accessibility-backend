c304096f55288efe35117d71eb1c9e69d855ef03
Alexandre Pereira <alexandre.pereira@pipecodes.com>
2024-05-10 17:41:01 +0100

    county feature
    county feature

--- resources/views/filament/pages/place-rate-settings.blade.php
+++ resources/views/filament/pages/place-rate-settings.blade.php
@@ -1,11 +1,6 @@
 <x-filament::page>
-	<x-filament::card>
-		<livewire:country-responsible-form />
-	</x-filament::card>
-	
-	<hr />
 
-	<x-filament::card>
-		<livewire:rate-question-form />
-	</x-filament::card>
+    <x-filament::card>
+        <livewire:rate-question-form />
+    </x-filament::card>
 </x-filament::page>
